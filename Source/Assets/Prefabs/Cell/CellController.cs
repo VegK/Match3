69a47d6f4a361f75b50cc9bc3420a8ee2f24ccc6
VegK <eal.prog@gmail.com>
2015-09-28 15:50:51 +1100

    Заготовка.

---
+++ Source/Assets/Prefabs/Cell/CellController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellController : MonoBehaviour
+{
+	#region Properties
+	#region Public
+	
+	#endregion
+	#region Private
+
+	#endregion
+	#endregion
+
+	#region Methods
+	#region Public
+
+	#endregion
+	#region Private
+	
+	#endregion
+	#endregion
+}
